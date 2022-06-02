3ec9479c56848fd0335ee8f16ea82edb180c81f9
Piotr Skowroński <47376245+H4wk507@users.noreply.github.com>
2022-06-02 15:49:27 +0200

    added game_over message, and menu refactor (#6)
    * Add GitHub Action to run `clang-format` on every PR (#3)
    
    * add clang-format check step
    
    * added game_over message, and menu refactor
    
    * fix comment, add const functions
    
    * Update prepare.yaml
    
    Co-authored-by: Bartek Pacia <barpac02@gmail.com>

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,26 +1,28 @@
 #include "../include/menu.h"
 
-Menu::Menu(double width, double height) {
+Menu::Menu(double width_, double height_, sf::Font font_)
+    : width(width_), height(height_), font(font_) {
     m_menu.resize(action_count);
     const std::vector<std::string> options = {"Play", "Info", "Quit"};
-    const int avg_char_width = 5;
-
-    if (!font.loadFromFile("arial.ttf")) {
-        std::clog << "snake: error loading font file" << std::endl;
-    }
 
     for (size_t i = 0; i < action_count; i++) {
-        double action_width = width / 2 - options[i].size() * avg_char_width;
-        double action_height = height / (action_count + 1) * (i + 1);
-
         m_menu[i].setFont(font);
         m_menu[i].setString(options[i]);
+
+        double action_width = (width - m_menu[i].getLocalBounds().width) / 2;
+        double action_height = height / (action_count + 1) * (i + 1);
+
         m_menu[i].setPosition(sf::Vector2f(action_width, action_height));
         m_menu[i].setFillColor(i == 0 ? sf::Color::Red : sf::Color::White);
     }
 }
 
-void Menu::open_info(float width, float height, sf::RenderWindow& window) {
+void Menu::open_info(sf::RenderWindow& window) const {
+    sf::Color midnight_blue(1, 3, 50);
+    window.clear(midnight_blue);
+    draw_text(0 * width, 0 * height, "CONTROLS:\n", window);
+    window.display();
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -38,15 +40,6 @@ void Menu::open_info(float width, float height, sf::RenderWindow& window) {
                     break;
             }
         }
-
-        sf::Color midnight_blue(1, 3, 50);
-        window.clear(midnight_blue);
-        draw_text(0 * width, 0 * height,
-                  "no i ja sie pytam czlowieku dumny ty \njestes z siebie "
-                  "zdajesz sobie \nsprawe z tego co robisz?\nmasz ty wogole "
-                  "rozum i godnosc czlowieka?",
-                  window);
-        window.display();
     }
 }
 
